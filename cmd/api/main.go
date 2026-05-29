2a4e3300db2c0fd241ec11b6d434e6f2804cfda2
yardow <yaredyilma11@gmail.com>
2026-05-29 17:07:03 +0300

    feat: chi and httplog

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/httplog/v3"
+)
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+}
+
+func main() {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	isLocalhost := os.Getenv("ENV") == "localhost"
+	logFormat := httplog.SchemaECS.Concise(isLocalhost)
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		ReplaceAttr: logFormat.ReplaceAttr,
+	}))
+
+	app := &application{
+		config: cfg,
+	}
+
+	r := chi.NewRouter()
+	r.Use(httplog.RequestLogger(logger, &httplog.Options{
+		Level:         slog.LevelInfo,
+		Schema:        logFormat,
+		RecoverPanics: true,
+	}))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	http.ListenAndServe(fmt.Sprintf(":%d", app.config.port), r)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+go 1.26.3
+
+require (
+	github.com/go-chi/chi/v5 v5.3.0
+	github.com/go-chi/httplog/v3 v3.3.0
+)
+
+module github.com/yaredow/glimpse-api
